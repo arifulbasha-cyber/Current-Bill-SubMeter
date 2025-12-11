e274b210180f55412a5e2a5dcf8eda8b4739be64
arifulbashar1 <arifulbashar1@gmail.com>
2025-12-11 18:00:21 +0000

    1 commit

---
+++ android/app/src/main/java/com/tmss/billsplitter/MainActivity.java
@@ -0,0 +1,5 @@
+package com.tmss.billsplitter;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.tmss.billsplitter',
+  appName: 'tmss-bill-splitter',
+  webDir: 'dist'
+};
+
+export default config;
